d74b3d0b276be034a1e318ae4a6e50121d16e96e
Anuka Hettiarachchi <anuka3382@gmail.com>
2024-05-22 20:12:27 +0530

    END 2020/2021 Paper 2 Q1

---
+++ src/End2021/Q1/Main.java
@@ -0,0 +1,12 @@
+package End2021.Q1;
+
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue pq = new PriorityQueue();
+        int[] numbers = {1245, 2345, 13324, 345, 3356, 2456, 6743, 56444, 66633, 234, 5467, 3445};
+        for (int i = 0; i <numbers.length ; i++) {
+            pq.insert(numbers[i]);
+        }
+        pq.display();
+    }
+}
